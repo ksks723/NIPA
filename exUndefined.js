f0179cfb5360fe7b3a602b34d2ce669de70f333a
ksks723 <ksks723@gmail.com>
2023-07-25 23:35:53 +0900

    unde vs null

---
+++ exUndefined.js
@@ -0,0 +1,8 @@
+let unde;
+console.log(unde);//아무 값이 없기에 undefined가 나온다. 선언만 하고 할당하지 않은상태이다. 
+let unde2 = undefined;
+console.log(unde2);//언디파인드를 할당하여 언디파인드가 나온다. 
+let nal=null;
+console.log(nal)//널을 할당하여 널이 나온다.
+let nal2 = {a:null}
+console.log(nal2)//마찬가지로 널이 나온다. 
